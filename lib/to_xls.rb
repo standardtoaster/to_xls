36dc60f199a22fd1693e7312720115b3570df911
Andrew Preece <andrew.preece@gmail.com>
2011-07-20 01:16:44 -0600

    Added mime type declaration

--- lib/to_xls.rb
+++ lib/to_xls.rb
@@ -1,3 +1,5 @@
+Mime::Type.register "application/vnd.ms-excel", :xls
+
 class Array
   def to_xls(options = {})
     output = '<?xml version="1.0" encoding="UTF-8"?><Workbook xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns:ss="urn:schemas-microsoft-com:office:spreadsheet" xmlns:html="http://www.w3.org/TR/REC-html40" xmlns="urn:schemas-microsoft-com:office:spreadsheet" xmlns:o="urn:schemas-microsoft-com:office:office"><Worksheet ss:Name="Sheet1"><Table>'
